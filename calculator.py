afe1613fd108d33f53c0fe23b464180a4e7ceb39
Juan Lozada <juandavidloza@gmail.com>
2024-09-09 17:03:55 -0300

    adicionando los archivos

---
+++ calculator.py
@@ -0,0 +1,43 @@
+def add (a,b):
+    return a + b
+
+def substract (a,b):
+    return a - b
+
+def multiply (a,b):
+    return a * b
+
+def divide (a,b):
+    return a / b
+
+def calculator():
+    while True:
+        print("Seleccione una operación")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Multiplicación")
+        print("4. División")
+        print("5. Salir")
+        
+        option= input("Ingrese su opción (1,2,3,4,5): ")
+        
+        if option =="5":
+            print ("Saliendo de la calculadora")
+            break
+
+        if option in ["1","2","3","4"]:
+            num1 = int(input("Ingrese el primer numero: "))
+            num2 = int(input("Ingrese el segundo numero: "))
+            
+            if option == "1":
+                print("La suma es: ",add(num1,num2))
+            elif option == "2":
+                print("La resta es: ", substract(num1,num2))
+            elif option == "3":
+                print("La multiplicación es: ", multiply(num1,num2))
+            elif option == "4":
+                print("La división es: ", divide(num1,num2))
+        else:
+            print("Opción no valida, por favor intenta de nuevo")
+
+calculator()
